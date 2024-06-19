d7cae4f4daf11145fb4594598d8728d831320c92
qiaozhididi <1343013603@qq.com>
2024-06-19 21:36:33 +0800

    使用插件实现pdf预览

--- src/views/TunnelInfo/PDFViewer/index.vue
+++ src/views/TunnelInfo/PDFViewer/index.vue
@@ -1,25 +1,27 @@
 <template>
   <!-- <embed :src="pdfUrl" type="application/pdf" width="100%" height="100%">  -->
-    <iframe :src="pdfUrl" width="100%" height="100%"></iframe>
-  <!-- <VuePdf v-for="page in numOfPages" :key="page" :src="pdfUrl"></VuePdf> -->
+  <!-- <iframe :src="pdfUrl" width="100%" height="100%"></iframe> -->
+  <VuePdf v-for="page in numOfPages" :key="page" :src="pdfUrl"></VuePdf>
 </template>
 
 <script setup>
 import api from "@/api/index";
 import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
-// import { VuePdf,createLoadingTask } from "vue3-pdfjs/esm";
-// import {VuePdfPropsType } from "vue3-pdfjs/components/vue-pdf-props/";
-// import {PDFDocumentProxy} from "pdfjs-dist/types/display/api";
+import { VuePdf,createLoadingTask } from "vue3-pdfjs/esm";
 
 const route = useRoute();
 const pdfUrl = ref("");
+const numOfPages = ref(0);
 
 onMounted(() => {
   api.getPDFView({ id: route.params.id }).then((res) => {
     if (res.data.status === 200) {
       pdfUrl.value = "http://localhost:3000/" + res.data.result[0].urlpath;
-      console.log(pdfUrl.value);
+      const loadingTask = createLoadingTask(pdfUrl.value);
+      loadingTask.promise.then((pdf) => {
+        numOfPages.value = pdf.numPages;
+      });
     }
   });
 });
